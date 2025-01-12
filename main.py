d6e19344342175dc056d2fb62183a28ad279ff12
takuaki <tatennisku@gmail.com>
2025-01-12 16:20:28 +0900

    third

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def main():
     USER_ID = info["USER_ID"]
-    messages = TextSendMessage(text="おはよう〜\n 朝だよ、起きてね❤️")
+    messages = TextSendMessage(text="おはよう〜\n 朝だよ、起きてね❤️起きろーーーー")
     line_bot_api.push_message(USER_ID, messages=messages)
 
 
